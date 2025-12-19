2f773a2aa0f305eda222ca3c755fa23e80ea5b37
arfan-rfn <arfan.rfn@gmail.com>
2025-12-19 00:45:15 -0700

    added global exception handler to the train-ticket microservice

---
+++ ts-order-service/src/main/java/com/cloudhubs/trainticket/order/exception/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.cloudhubs.trainticket.order.exception;
+
+import com.cloudhubs.trainticket.order.util.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Response> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
+        LOGGER.error("[GlobalExceptionHandler][JSON parse error: {}]", ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new Response<>(0, "Invalid request body: " + ex.getMostSpecificCause().getMessage(), null));
+    }
+
+    @ExceptionHandler(TokenException.class)
+    public ResponseEntity<Response> handleTokenException(TokenException ex) {
+        LOGGER.error("[GlobalExceptionHandler][Token error: {}]", ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(new Response<>(0, ex.getMessage(), null));
+    }
+}
